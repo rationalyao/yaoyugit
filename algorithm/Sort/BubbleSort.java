33372cf879d05b3d9d22a4ad48838cb794fea489
yaoyu_pro <yaoyu673694777@gmail.com>
2019-07-14 03:26:42 +0800

    Sort out the file BubbleSort.java

--- algorithm/Sort/BubbleSort.java
+++ algorithm/Sort/BubbleSort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class test{
+public class BubbleSort{
     public static void main(String[] args) {
         int[] arr_test = {1,3154,45,2,6,4,1,52,6,326,43};
         System.out.println(Arrays.toString(BubbleSort(arr_test)));      
@@ -12,7 +12,7 @@ public class test{
 
         for (int i = 0;i < length - 1; i++) {
              for (int j = 0; j < length - i - 1 ; j ++) {
-                if (arr[j] < arr[j + 1]) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
