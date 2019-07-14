0a061b0a187a6c182cc1cb7c35b8bacb40159e1d
yaoyu_pro <yaoyu673694777@gmail.com>
2019-07-14 16:04:22 +0800

    Sort out the file InsertSort

--- algorithm/Sort/InsertSort.java
+++ algorithm/Sort/InsertSort.java
@@ -9,16 +9,12 @@ public class InsertSort {
 	public static int[] InsertSort(int[] arr) {
 		for (int i = 1; i < arr.length; i++) {
 			for (int j = i; j > 0; j--) {
-
 				if (arr[j-1] > arr[j]) {
-
 					int temp = arr[j];
 					arr[j] = arr[j-1];
 					arr[j-1] = temp;
-					
 				}
 			}
-			
 		}
 		return arr;
 	}
