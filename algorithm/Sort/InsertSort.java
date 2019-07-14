e47efb093bff81220cb3d4819a82d5377540ab14
yaoyu_pro <yaoyu673694777@gmail.com>
2019-07-14 15:52:34 +0800

    add new file InsertSort

---
+++ algorithm/Sort/InsertSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class InsertSort {
+	public static void main(String[] args) {
+		int[] arr_test = {1,23,6,41,245,2,5635,465,357,5865,3,6256345};
+		System.out.println(Arrays.toString(InsertSort(arr_test)));
+	}
+
+	public static int[] InsertSort(int[] arr) {
+
+		for (int i = 1; i < arr.length; i++) {
+
+			for (int j = i; j > 0; j--) {
+
+				if (arr[j-1] > arr[j]) {
+
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+					
+				}
+			}
+			
+		}
+
+		return arr;
+	}
+}
